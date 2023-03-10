fca5384895ea6faf57cf33ddaf1d96c2cfbe8ad2
Jimmy Chan <jchan85@gatech.edu>
2023-03-10 14:21:44 -0800

    Included: astronaut model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
+// import { ComputersCanvas } from './canvas';
+
+import { AstronautCanvas } from './canvas';
 
 const Hero = () => {
   return (
@@ -25,7 +27,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <AstronautCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,13 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
+import EarthCanvas from './Earth';
+import BallCanvas from './Ball';
+import ComputersCanvas from './Computers';
+import StarsCanvas from './Stars';
+import AstronautCanvas from './Astronaut';
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export {
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+  AstronautCanvas,
+};
