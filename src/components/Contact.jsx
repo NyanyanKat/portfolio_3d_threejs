cbd93f0d1bd41102a6a5a84f2e7809a6b4b5ca62
Jimmy Chan <jchan85@gatech.edu>
2023-03-10 02:00:12 -0800

    bug fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,16 +36,16 @@ const Contact = () => {
 
     // emailjs
     //   .send(
-    //     'service_d27joxw',
-    //     'template_s23w5uf',
-    //     {
-    //       from_name: form.name,
-    //       to_name: 'Jimmy',
-    //       from_email: form.email,
-    //       to_email: 'ylchan@uci.edu',
-    //       message: form.message,
-    //     },
-    //     'Z_2yk4B8Z8Sa-A9Zp'
+    //    import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    //    import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    //    {
+    //      from_name: form.name,
+    //      to_name: 'Jimmy',
+    //      from_email: form.email,
+    //      to_email: 'ylchan@uci.edu',
+    //      message: form.message,
+    //    },
+    //    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     //   )
     //   .then(
     //     () => {
